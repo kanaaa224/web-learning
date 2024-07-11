b435c7ee22fb6a9742c9d07cb91169d1aa6ac1f3
Kanato Shimabukuro <kanaaa224@gmail.com>
2024-07-12 00:05:57 +0900

    [ optimize ] タイピングゲーム開発の途中経過

--- typingGame/src/main.js
+++ typingGame/src/main.js
@@ -1,29 +1,57 @@
+/*
+    タイピングゲーム アプリケーションクラス
+
+    作成者: 島袋 叶望
+    作成日: 24/07/11
+
+    必要な外部ライブラリ: なし
+
+    内容: タイピングゲーム（問題はjsonファイルから読みこむ）
+*/
+
 class App {
+    // コンストラクター
     constructor() {
+        this.eventHandlers = []; // イベントハンドラー管理用
+        this.intervals     = []; // インターバル管理用
+
         // ページのロードが完了したとき、タイトル画面を描画する関数を実行する
         window.addEventListener('load', () => {
-            this.drawTitle();
+            this.title();
         });
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // jsonファイルを読み込む
     fetchJSON(filePath = '', callback = () => {}) {
         fetch(filePath)
             .then(response => response.json())
-            .then(data     => callback(data))
-            .catch(error   => console.error('fetching error: ', error));
+            .then(data => callback(data))
+            .catch(error => console.error('fetching error: ', error));
 
         return true;
     }
 
     // 「1」を「01」にしてくれる関数
     twoDigit(num) {
-        if(num < 10) return ('0' + num);
-        else return num;
+        return num < 10 ? '0' + num : num.toString();
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     // タイトル
-    drawTitle() {
+    title() {
+        // 全てのイベントハンドラーを解除（この関数の前に実行された関数でリスナーされたイベントのハンドラーは全て不要なため）
+        if(this.eventHandlers.length) {
+            for(let i = 0; i < this.eventHandlers.length; i++) {
+                if(this.eventHandlers[i]) document.removeEventListener(this.eventHandlers[i].typeName, this.eventHandlers[i].callback);
+            }
+
+            this.eventHandlers = [];
+        }
+
+        // UIを構築し描画
         let titleElement     = '<div class="title"><h1>タイピングゲーム</h1><button onclick="app.playTypingGame();">ゲーム開始</button></div>';
         let containerElement = '<div class="container">' + titleElement + '</div>';
         let mainElement      = '<main>' + containerElement + '</main>';
@@ -33,80 +61,130 @@ class App {
         return true;
     }
 
-    // タイピングゲームを開始
+    // タイピングゲームを準備
     playTypingGame() {
-        // タイピングゲームの出題データがない場合、ファイルをフェッチして自分をコールバックする
+        // タイピングゲームのデータがない場合、出題データファイルをフェッチして自分をコールバックする
         if(!this.typingGame_data) {
             return this.fetchJSON('./res/questionContents.json', (data) => {
                 this.typingGame_data = { 'questionContents': data };
 
-                return this.playTypingGame();
+                this.playTypingGame();
             });
         }
 
         // UIを構築し描画
-        let typingGameElement = '<div class="typingGame"><div class="time">00:00.00</div><h2>Enterキーを押すとゲームが始まります。</h2><button onclick="app.drawTitle();">やめる</button></div>';
+        let typingGameElement = '<div class="typingGame"><div class="time">00:00.00</div><h2>Enterキーを押すとゲームが始まります。</h2><p></p><button onclick="app.title();">やめる</button></div>';
         let containerElement  = '<div class="container">' + typingGameElement + '</div>';
         let mainElement       = '<main>' + containerElement + '</main>';
 
         document.querySelector('body').innerHTML = mainElement;
 
-        // イベント追加
-        const keydownHandler = event => {
-            if(event.key === 'Enter') {
-                document.removeEventListener('keydown', keydownHandler);
+        // キー入力イベントを追加
+        let eventHandler = {
+            'typeName': 'keydown',
+            'callback': event => {
+                if(event.key === 'Enter') {
+                    this.typingGame_data.time = [ 0, 0, 0 ];
 
-                this.typingGame_data.time = [ 0, 0, 0 ];
+                    this.typingGame_data.typingCount = 0;
+                    this.typingGame_data.missCount   = 0;
+                    this.typingGame_data.clearCount  = 0;
 
-                this.typingGame_data.typingCount = 0;
-                this.typingGame_data.missCount   = 0;
-                this.typingGame_data.clearCount  = 0;
+                    this.typingGame_data.currentQuestionIndex = 0;
 
-                this.typingGame_data.interval = [];
+                    this.typingGame_data.answer = [];
 
-                this.typingGame();
+                    this.startTypingGame();
+                }
             }
         };
-        
-        document.addEventListener('keydown', keydownHandler);
+
+        this.eventHandlers[0] = eventHandler;
+
+        document.addEventListener(eventHandler.typeName, eventHandler.callback);
 
         return true;
     }
 
-    // タイピングゲーム
-    typingGame() {
-        // タイマーのインターバルを登録
-        this.typingGame_data.interval.push(setInterval(() => {
+    // タイピングゲームを開始
+    startTypingGame() {
+        // 全てのイベントハンドラーを解除（この関数の前に実行された関数でリスナーされたイベントのハンドラーは全て不要なため）
+        if(this.eventHandlers.length) {
+            for(let i = 0; i < this.eventHandlers.length; i++) {
+                document.removeEventListener(this.eventHandlers[i].typeName, this.eventHandlers[i].callback);
+            }
+
+            this.eventHandlers = [];
+        }
+
+        // キー入力イベントを追加
+        let eventHandler = {
+            'typeName': 'keydown',
+            'callback': event => {
+                if(event.key === 'Escape') return this.endTypingGame();
+                console.log(event.keyCode);
+            }
+        };
+
+        this.eventHandlers[1] = eventHandler;
+
+        document.addEventListener(eventHandler.typeName, eventHandler.callback);
+
+        document.querySelector('.typingGame h2').innerHTML = this.typingGame_data.questionContents[0].string;
+        //for(let i = 0; i < this.typingGame_data.questionContents[0])
+
+        // タイムを表示するため更新と表示のインターバルを登録
+        this.intervals.push(setInterval(() => {
             this.typingGame_data.time[0]++;
         }, 60 * 1000));
 
-        this.typingGame_data.interval.push(setInterval(() => {
+        this.intervals.push(setInterval(() => {
             this.typingGame_data.time[1]++;
             if(this.typingGame_data.time[1] >= 60) this.typingGame_data.time[1] = 0;
         }, 1000));
 
-        this.typingGame_data.interval.push(setInterval(() => {
+        this.intervals.push(setInterval(() => {
             this.typingGame_data.time[2]++;
             if(this.typingGame_data.time[2] >= 100) this.typingGame_data.time[2] = 0;
-        }, 10));
 
-        this.typingGame_data.interval.push(setInterval(() => {
             document.querySelector('.typingGame .time').innerHTML = this.twoDigit(this.typingGame_data.time[0]) + ':' + this.twoDigit(this.typingGame_data.time[1]) + '.' + this.twoDigit(this.typingGame_data.time[2]);
         }, 10));
 
+        // UIを更新
+        document.querySelector('.typingGame p') .innerHTML = '';
+
         document.querySelector('.typingGame button').setAttribute('onclick', 'app.endTypingGame();');
+        document.querySelector('.typingGame button').innerHTML = '終わる(Escapeキーでも終了できます)';
 
         return true;
     }
 
     // タイピングゲームを終了
     endTypingGame() {
-        // タイマーのインターバルを解除
-        for(let i = 0; i < this.typingGame_data.interval.length; i++) {
-            clearInterval(this.typingGame_data.interval[i]);
+        // 全てのイベントハンドラーを解除（この関数の前に実行された関数でリスナーされたイベントのハンドラーは全て不要なため）
+        if(this.eventHandlers.length) {
+            for(let i = 0; i < this.eventHandlers.length; i++) {
+                if(this.eventHandlers[i]) document.removeEventListener(this.eventHandlers[i].typeName, this.eventHandlers[i].callback);
+            }
+
+            this.eventHandlers = [];
         }
 
-        document.querySelector('.typingGame button').setAttribute('onclick', 'app.drawTitle();');
+        // タイマーのインターバルを解除
+        if(this.intervals.length) {
+            for(let i = 0; i < this.intervals.length; i++) {
+                clearInterval(this.intervals[i]);
+            }
+
+            this.intervals = [];
+        }
+
+        // UIを更新
+        document.querySelector('.typingGame h2').innerHTML = 'クリア数: ' + this.typingGame_data.currentQuestionIndex + '/' + this.typingGame_data.questionContents.length;
+        document.querySelector('.typingGame p') .innerHTML = ('総タイプ数: ' + this.typingGame_data.typingCount) + '<br>' + ('ミス数: ' + this.typingGame_data.missCount) + '<br>' + ('当たり数: ' + this.typingGame_data.clearCount);
+
+        document.querySelector('.typingGame button').setAttribute('onclick', 'app.title();');
+        document.querySelector('.typingGame button').innerHTML = 'タイトルへ';
 
         return true;
     }
