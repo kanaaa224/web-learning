9d952e82c33c0eab653bfdfe16263e998f657acb
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2024-09-02 10:52:51 +0900

    [ update ] タイピングゲーム オフライン環境版 分かりやすいように更新

--- typingGame_offline/src/main.js
+++ typingGame_offline/src/main.js
@@ -15,6 +15,8 @@ class App {
         this.eventHandlers = []; // イベントハンドラー管理用
         this.intervals     = []; // インターバル管理用
 
+        //this.contentsDataFilePath = './res/questionContents.json'; // 問題データファイルのパス
+
         // ページのロードが完了したとき、タイトル画面を描画する関数を実行する
         window.addEventListener('load', () => {
             this.title();
@@ -22,6 +24,17 @@ class App {
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // jsonファイルを読み込む（読み込みが成功したら引数で渡された関数を実行する）
+    /* fetchJSON(filePath = '', callback = () => {}) {
+        // FetchAPI | 非同期（リロードせずに）にネットワーク越しでリソースを取得できる
+        fetch(filePath)
+            .then(response => response.json())
+            .then(data => callback(data))
+            .catch(error => console.error(`fetching error: ${error}`));
+
+        return true;
+    } */
 
     // 「1」を「01」にしてくれる関数
     twoDigit(num) {
@@ -55,341 +68,24 @@ class App {
     playTypingGame() {
         // タイピングゲームのデータがない場合、出題データファイルをフェッチして自分をコールバックする
         if(!this.typingGame_data) {
-            this.typingGame_data = {
-                questionContents: [
-                    {
-                        "string": "吾輩は猫である",
-                        "characters": [
-                            {
-                                "text": "吾",
-                                "hiragana": "わが",
-                                "katakana": "ワガ",
-                                "romaji": "waga"
-                            },
-                            {
-                                "text": "輩",
-                                "hiragana": "はい",
-                                "katakana": "ハイ",
-                                "romaji": "hai"
-                            },
-                            {
-                                "text": "は",
-                                "hiragana": "は",
-                                "katakana": "ハ",
-                                "romaji": "ha"
-                            },
-                            {
-                                "text": "猫",
-                                "hiragana": "ねこ",
-                                "katakana": "ネコ",
-                                "romaji": "neko"
-                            },
-                            {
-                                "text": "で",
-                                "hiragana": "で",
-                                "katakana": "デ",
-                                "romaji": "de"
-                            },
-                            {
-                                "text": "あ",
-                                "hiragana": "あ",
-                                "katakana": "ア",
-                                "romaji": "a"
-                            },
-                            {
-                                "text": "る",
-                                "hiragana": "る",
-                                "katakana": "ル",
-                                "romaji": "ru"
-                            }
-                        ]
-                    },
-                    {
-                        "string": "雨ニモマケズ風ニモマケズ",
-                        "characters": [
-                            {
-                                "text": "雨",
-                                "hiragana": "あめ",
-                                "katakana": "アメ",
-                                "romaji": "ame"
-                            },
-                            {
-                                "text": "ニ",
-                                "hiragana": "に",
-                                "katakana": "ニ",
-                                "romaji": "ni"
-                            },
-                            {
-                                "text": "モ",
-                                "hiragana": "も",
-                                "katakana": "モ",
-                                "romaji": "mo"
-                            },
-                            {
-                                "text": "マ",
-                                "hiragana": "ま",
-                                "katakana": "マ",
-                                "romaji": "ma"
-                            },
-                            {
-                                "text": "ケ",
-                                "hiragana": "け",
-                                "katakana": "ケ",
-                                "romaji": "ke"
-                            },
-                            {
-                                "text": "ズ",
-                                "hiragana": "ず",
-                                "katakana": "ズ",
-                                "romaji": "zu"
-                            },
-                            {
-                                "text": "風",
-                                "hiragana": "かぜ",
-                                "katakana": "カゼ",
-                                "romaji": "kaze"
-                            },
-                            {
-                                "text": "ニ",
-                                "hiragana": "に",
-                                "katakana": "ニ",
-                                "romaji": "ni"
-                            },
-                            {
-                                "text": "モ",
-                                "hiragana": "も",
-                                "katakana": "モ",
-                                "romaji": "mo"
-                            },
-                            {
-                                "text": "マ",
-                                "hiragana": "ま",
-                                "katakana": "マ",
-                                "romaji": "ma"
-                            },
-                            {
-                                "text": "ケ",
-                                "hiragana": "け",
-                                "katakana": "ケ",
-                                "romaji": "ke"
-                            },
-                            {
-                                "text": "ズ",
-                                "hiragana": "ず",
-                                "katakana": "ズ",
-                                "romaji": "zu"
-                            }
-                        ]
-                    },
-                    {
-                        "string": "こんにちは、世界！",
-                        "characters": [
-                            {
-                                "text": "こ",
-                                "hiragana": "こ",
-                                "katakana": "",
-                                "romaji": "ko"
-                            },
-                            {
-                                "text": "ん",
-                                "hiragana": "ん",
-                                "katakana": "",
-                                "romaji": "nn"
-                            },
-                            {
-                                "text": "に",
-                                "hiragana": "に",
-                                "katakana": "",
-                                "romaji": "ni"
-                            },
-                            {
-                                "text": "ち",
-                                "hiragana": "ち",
-                                "katakana": "",
-                                "romaji": "ti"
-                            },
-                            {
-                                "text": "は",
-                                "hiragana": "は",
-                                "katakana": "",
-                                "romaji": "ha"
-                            },
-                            {
-                                "text": "、",
-                                "hiragana": "、",
-                                "katakana": "",
-                                "romaji": ","
-                            },
-                            {
-                                "text": "世",
-                                "hiragana": "せ",
-                                "katakana": "",
-                                "romaji": "se"
-                            },
-                            {
-                                "text": "界",
-                                "hiragana": "かい",
-                                "katakana": "",
-                                "romaji": "kai"
-                            },
-                            {
-                                "text": "!",
-                                "hiragana": "!",
-                                "katakana": "!",
-                                "romaji": "!"
-                            }
-                        ]
-                    },
-                    {
-                        "string": "早起きは三文の徳",
-                        "characters": [
-                            {
-                                "text": "早",
-                                "hiragana": "はや",
-                                "katakana": "ハヤ",
-                                "romaji": "haya"
-                            },
-                            {
-                                "text": "起",
-                                "hiragana": "お",
-                                "katakana": "オ",
-                                "romaji": "o"
-                            },
-                            {
-                                "text": "き",
-                                "hiragana": "き",
-                                "katakana": "キ",
-                                "romaji": "ki"
-                            },
-                            {
-                                "text": "は",
-                                "hiragana": "は",
-                                "katakana": "ハ",
-                                "romaji": "ha"
-                            },
-                            {
-                                "text": "三",
-                                "hiragana": "さん",
-                                "katakana": "サン",
-                                "romaji": "sann"
-                            },
-                            {
-                                "text": "文",
-                                "hiragana": "もん",
-                                "katakana": "モン",
-                                "romaji": "monn"
-                            },
-                            {
-                                "text": "の",
-                                "hiragana": "の",
-                                "katakana": "ノ",
-                                "romaji": "no"
-                            },
-                            {
-                                "text": "徳",
-                                "hiragana": "とく",
-                                "katakana": "トク",
-                                "romaji": "toku"
-                            }
-                        ]
-                    },
-                    {
-                        "string": "こんにちは、今日は良い天気ですね",
-                        "characters": [
-                            {
-                                "text": "こ",
-                                "hiragana": "こ",
-                                "katakana": "",
-                                "romaji": "ko"
-                            },
-                            {
-                                "text": "ん",
-                                "hiragana": "ん",
-                                "katakana": "",
-                                "romaji": "nn"
-                            },
-                            {
-                                "text": "に",
-                                "hiragana": "に",
-                                "katakana": "",
-                                "romaji": "ni"
-                            },
-                            {
-                                "text": "ち",
-                                "hiragana": "ち",
-                                "katakana": "",
-                                "romaji": "ti"
-                            },
-                            {
-                                "text": "は",
-                                "hiragana": "は",
-                                "katakana": "",
-                                "romaji": "ha"
-                            },
-                            {
-                                "text": "、",
-                                "hiragana": "、",
-                                "katakana": "",
-                                "romaji": ","
-                            },
-                            {
-                                "text": "今日",
-                                "hiragana": "きょう",
-                                "katakana": "",
-                                "romaji": "kyou"
-                            },
-                            {
-                                "text": "は",
-                                "hiragana": "は",
-                                "katakana": "",
-                                "romaji": "ha"
-                            },
-                            {
-                                "text": "良",
-                                "hiragana": "い",
-                                "katakana": "",
-                                "romaji": "i"
-                            },
-                            {
-                                "text": "い",
-                                "hiragana": "い",
-                                "katakana": "",
-                                "romaji": "i"
-                            },
-                            {
-                                "text": "天",
-                                "hiragana": "てん",
-                                "katakana": "",
-                                "romaji": "tenn"
-                            },
-                            {
-                                "text": "気",
-                                "hiragana": "き",
-                                "katakana": "",
-                                "romaji": "ki"
-                            },
-                            {
-                                "text": "で",
-                                "hiragana": "で",
-                                "katakana": "",
-                                "romaji": "de"
-                            },
-                            {
-                                "text": "す",
-                                "hiragana": "す",
-                                "katakana": "",
-                                "romaji": "su"
-                            },
-                            {
-                                "text": "ね",
-                                "hiragana": "ね",
-                                "katakana": "",
-                                "romaji": "ne"
-                            }
-                        ]
-                    }
-                ]
-            };
+            // httpではなくfileのプロトコルによりfetchが使えない状況なので、
+            // jsonではなくjsの変数に保存する方法でデータを取得
+
+            /* return this.fetchJSON(this.contentsDataFilePath, (data) => {
+                this.typingGame_data = { questionContents: data };
+
+                this.playTypingGame();
+            }); */
+
+            // typingGame_contents変数が存在し、中身があるかチェックする
+            // 無ければ通知し、falseを返す
+            if(typeof typingGame_contents === 'undefined' || typingGame_contents === null) {
+                alert('問題データが見つかりません');
+
+                return false;
+            }
+
+            this.typingGame_data = { questionContents: typingGame_contents };
 
             return this.playTypingGame();
         }
@@ -567,18 +263,18 @@ class App {
             this.intervals.push(setInterval(() => {
                 this.typingGame_data.time[0]++; // min
             }, 60 * 1000));
-    
+
             // 秒の更新
             this.intervals.push(setInterval(() => {
                 this.typingGame_data.time[1]++; // sec
                 if(this.typingGame_data.time[1] >= 60) this.typingGame_data.time[1] = 0;
             }, 1000));
-    
+
             // コンマ秒の更新と表示
             this.intervals.push(setInterval(() => {
                 this.typingGame_data.time[2]++; // sec point (00:00.XX)
                 if(this.typingGame_data.time[2] >= 100) this.typingGame_data.time[2] = 0;
-    
+
                 document.querySelector('.typingGame .time').innerHTML = `${this.twoDigit(this.typingGame_data.time[0])}:${this.twoDigit(this.typingGame_data.time[1])}.${this.twoDigit(this.typingGame_data.time[2])}`;
             }, 10));
         }
